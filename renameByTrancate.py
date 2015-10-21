dae361d1029e6d04bde582b30f57076854bdc8e1
tuouo <ltytuotuo@gmail.com>
2015-10-22 00:24:45 +0800

    os.chdir os.rename

---
+++ renameByTrancate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+#cur = os.getcwd()						# get current path 
+path = r'C:\Users\tuouo_000\gits\uikit\images'
+os.chdir(path)							# set current path 
+dirlist = os.listdir(path)
+for i in dirlist:
+    names = os.path.splitext(i)
+    name = names[0].split('_')[0] + names[1]    
+    os.rename(i, name)
