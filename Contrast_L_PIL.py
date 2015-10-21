a4f714d37d3e9cb1068a520d31463653b49d1384
tuouo <ltytuotuo@gmail.com>
2015-10-22 00:24:00 +0800

    ImageEnhanc.Contrast convert PIL

---
+++ Contrast_L_PIL.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# just test unskilled code
+
+##!/usr/bin/env python3
+## -*- coding: utf-8 -*-
+## 引用PIL中的模块 Image, ImageEnhance（python 3.X写法，下同）
+#from PIL import Image, ImageEnhance
+#
+## 打开当前文件夹下的名为‘123.png’的图片，如要打开其他图片，需要另外的语句
+#im = Image.open('123.png')
+#
+##获取对比度实例（色彩平衡，亮度平衡，对比度，锐化度：Color、Brightness、Contrast和Sharpness）
+#imEnhancer = ImageEnhance.Contrast(im)
+##循环显示改变对比度后的图片
+#for i in range(7):
+#    #factor = i / 4
+#    #imEnhancer.enhance(factor).show("Contrast %f" % factor)
+#
+#from PIL import Image, ImageEnhance
+#im = Image.open('123.png')
+#for i in range(7):
+#    #factor = i / 4
+#    imEnhancer.enhance(factor).show("Contrast %f" % factor)
+
+#from PIL import Image
+#im = Image.open('123.png')
+#imL = im.convert("L")	#转为灰度图像
+#imL.show()
